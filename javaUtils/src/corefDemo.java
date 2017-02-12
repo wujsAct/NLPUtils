8254dfbd925a2f1f4c7c399656a7e46d27391920
wujs <wujs@act.buaa.edu.cn>
2017-02-12 16:25:50 +0800

    utf-8

--- javaUtils/src/corefDemo.java
+++ javaUtils/src/corefDemo.java
@@ -1,13 +1,6 @@
-import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
-
-import edu.stanford.nlp.coref.CorefCoreAnnotations;
-import edu.stanford.nlp.coref.data.CorefChain;
-import edu.stanford.nlp.coref.data.CorefChain.CorefMention;
-import edu.stanford.nlp.coref.data.Mention;
 import edu.stanford.nlp.io.IOUtils;
-import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
