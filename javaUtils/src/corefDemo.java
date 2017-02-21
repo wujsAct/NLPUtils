cffb85c55896f17a6698564c651e15f1cc5fe2fd
wujs <wujs@act.buaa.edu.cn>
2017-02-21 19:43:27 +0800

    stanford deep nueral coreference process

--- javaUtils/src/corefDemo.java
+++ javaUtils/src/corefDemo.java
@@ -27,18 +27,26 @@ public class corefDemo {
 	public static String dataPath = dirPath + "msnbc/";
 	public static String fcorefret = dataPath+"corefRet.txt";
 	public static Properties props = new Properties();
+	//deep learning coreference is good 
 	static {
 		props.setProperty("annotators",
-				"tokenize,ssplit,pos,lemma,ner,parse,dcoref");
+				"tokenize,ssplit,pos,lemma,ner,parse,mention,coref");
 		props.setProperty("ssplit.eolonly", "true"); // one line is a sentence;
 														// we need to utilize
 														// this formation
+		props.setProperty("coref.algorithm", "neural");
 	}
 	public static StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
 	public static void main(String[] args) {
 		try {
 			getDoc();
+			/**
+			String fileN = dataPath + "smallData.txt";
+			String text = IOUtils.slurpFileNoExceptions(fileN);
+			Writer fcoref = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream(new File(fcorefret+"small"),true), "UTF8"));
+			getDocCoref(0, text, fcoref);**/
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -100,7 +108,28 @@ public class corefDemo {
 		}
 		return null;
 	}
-
+	public static String getRealMent(Integer docId,CorefMention m){
+		String mentToken = m.mentionSpan;
+		String mentStr;
+		if(mentToken.contains("'s") || mentToken.contains(",")){
+			String tokens = mentToken.split("'s|,")[0].trim();
+			Integer lent = tokens.split(" ").length;
+			mentStr = docId + "_"
+					+ Integer.toString(m.sentNum-1) + "\t"
+					+ Integer.toString(m.startIndex-1) + "\t"
+					+ Integer.toString(m.startIndex-1 +lent) +"\t"
+					+ tokens;
+		}
+		else{
+		//need to delete relative clause  's, or ,
+				mentStr = docId + "_"
+				+ Integer.toString(m.sentNum-1) + "\t"
+				+ Integer.toString(m.startIndex-1) + "\t"
+				+ Integer.toString(m.endIndex-1) +"\t"
+				+ m.mentionSpan;
+		}
+		return mentStr;
+	}
 	/**
 	 * @time 2017/2/12
 	 * @param text
@@ -112,32 +141,26 @@ public class corefDemo {
 		Annotation document = new Annotation(text);
 		pipeline.annotate(document);
 		List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-		// System.out.println("docId:"+docId+" senLent:"+sentences.size()+"\t"+text.split("\n").length);
+		 System.out.println("docId:"+docId+" senLent:"+sentences.size()+"\t"+text.split("\n").length);
 		// //one line for one sentence (ssplit)
-
+		if(sentences.size() != text.split("\n").length){
+			System.err.println("different sentence length");
+		}
 		for (CorefChain cc : document.get(
 				CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
 			// System.out.println("\t"+cc);
 			CorefMention repMent = cc.getRepresentativeMention();
-			String repMentStr = docId + "_" + Integer.toString(repMent.sentNum-1)
-					+ "\t" + Integer.toString(repMent.startIndex-1) + "\t"
-					+ Integer.toString(repMent.endIndex-1) + "\t"
-					+ repMent.mentionSpan + "\t" + repMent.mentionType;
-
-			System.out.println(repMentStr);
+			
 			List<CorefMention> ments = cc.getMentionsInTextualOrder();
-
+			String allments = "";
 			for (CorefMention m : ments) {
 				System.out.println(m);
-				String mentStr = docId + "_"
-						+ Integer.toString(m.sentNum-1) + "\t"
-						+ Integer.toString(m.startIndex-1) + "\t"
-						+ Integer.toString(m.endIndex-1) +"\t"
-						+ m.mentionSpan;
-				fcoref.flush();
-				fcoref.append(repMentStr+"\t\t"+mentStr+"\n");
-				fcoref.flush();
+				String mentStr = getRealMent(docId,m);
+				allments = allments + mentStr +"\t\t";
 			}
+			allments  = allments.trim();
+			fcoref.append(allments+"\n");
+			fcoref.flush();
 		}
 	}
 
